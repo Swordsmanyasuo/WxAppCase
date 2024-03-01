432f102772643ccdd558b39f92acc7d15095f8bb
Swordsmanyasuo <123147705+Swordsmanyasuo@users.noreply.github.com>
2024-03-01 14:38:38 +0800

    part2静态页面设计-分类页面

--- pages/classify/index.js
+++ pages/classify/index.js
@@ -1,66 +1,46 @@
-// pages/classify/index.js
+// 分类页面 shop/pages/classify/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    cateId:0,//当前分类的ID
+    windowHeight: 0,
+    windowWidth: 0,
+    categories: [
+      { categoryId: 0, cateName: '未分类'},
+      { categoryId: 1, cateName: '蔬菜类'},
+      { categoryId: 2, cateName: '水果类'},
+      { categoryId: 3, cateName: '农家'  },
+      { categoryId: 4, cateName: '家禽类'},
+      { categoryId: 5, cateName: '特产'  },
+      { categoryId: 6, cateName: '手工类'},
+      { categoryId: 7, cateName: '其他'  },
+    ],
+    goodsList: [
+      {
+      classify: "未分类", description: "好吃又正宗的兴国县鱼丝", name: "兴国鱼丝",
+      iconUrl:"https://p1.itc.cn/images01/20220326/9cd99528df884e968c54be71fbd6fe4a.jpeg",    
+      price:39,sales: 999,specification: "500g",status: 1,storage: 999,_id:'1'},
+      { 
+      classify: "未分类", description: "赣南脐橙果肉鲜嫩多汁", name: "脐橙",
+      iconUrl: "https://img2.baidu.com/it/u=3789706499,3442127525&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800",
+      price: 29.9, sales: 999, specification: "250g", status: 1,storage: 999,_id: "2"}]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  changeCate: function (e) {
+    this.setData({
+      cateId: e.target.dataset.id,
+    }),
+    wx.showToast({ title: '加载中..', icon: 'loading', duration: 1000 })
+    this.getGoods(this.limit, this.skip);//请求数据，传入需要的数据条数限制limit，以及
+    //起始点skip
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+  onShow: function (e) {
+    //用于设置商品滑动视图的宽高
+    wx.getSystemInfo({  //调用API函数
+      success: (res) => {
+        this.setData({
+          windowHeight: res.windowHeight,
+          windowWidth: res.windowWidth
+        })
+      }
+    })
+  },   
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,9 +6,11 @@ Page({
     current:0,
     interval:5000,
     duration:1000,
-    images:['https://ts1.cn.mm.bing.net/th/id/R-C.7ac5e746062d30c19a9423201ff09b68?rik=oQCeSWDPHcuMPg&riu=http%3a%2f%2fimg.fsonline.com.cn%2fa%2f10001%2f202006%2f9938f37a7dbe6292c5b7dd7ef66579a3.jpeg&ehk=u3DTtCF%2bJiphBXlR2CFqa%2bvzfc%2bFpdN%2briKjbuHdRIc%3d&risl=&pid=ImgRaw&r=0',
-		'https://ts1.cn.mm.bing.net/th/id/R-C.a8f84f03f3d4cbc222a39932e7cb48b2?rik=VBCfyBujCNQZ8g&riu=http%3a%2f%2fres.sgfb.sgxw.cn%2fa%2f10001%2f202009%2ffa944b14fbd403386fd1eb388ed7e6e4.png&ehk=urtirvdEHel85kkb1iLGwXalKAAHESw7clCCQkI6XZo%3d&risl=1&pid=ImgRaw&r=0',
-		'https://tse4-mm.cn.bing.net/th/id/OIP-C.fQjcSQM14r3RTunEquQ2IgHaE7?rs=1&pid=ImgDetMain'
+    images:[
+      '/images/swiper/1.jpg',
+      '/images/swiper/2.jpg',
+      '/images/swiper/3.jpg'
+
   ],
 
   // 分类数据
