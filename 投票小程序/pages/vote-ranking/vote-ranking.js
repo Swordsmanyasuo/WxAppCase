ef4e1e07534da36d9bcb994ed6427531226f1c7f
Swordsmanyasuo <123147705+Swordsmanyasuo@users.noreply.github.com>
2024-03-23 13:42:11 +0800

    完成页面布局

--- 投票小程序/pages/vote-ranking/vote-ranking.js
+++ 投票小程序/pages/vote-ranking/vote-ranking.js
@@ -1,66 +1,57 @@
-// pages/vote-ranking/vote-ranking.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage() {
+    return {
+      title: 'label',
+      path: 'page/component/pages/label/label'
+    }
+  },
 
+  data: {
+    isvote:true,//是否能投票
+    checkboxItems: [
+      {name: '牢大', value: '牢大'},
+      {name: '科比', value: '科比'}
+    ],
+    radioItems: [
+      {name: '牢大', value: '牢大'},
+      {name: '科比', value: '科比'}
+    ],
+    hidden: false
+  },
+
+  checkboxChange(e) {
+    const checked = e.detail.value
+    const changed = {}
+    for (let i = 0; i < this.data.checkboxItems.length; i++) {
+      if (checked.indexOf(this.data.checkboxItems[i].name) !== -1) {
+        changed['checkboxItems[' + i + '].checked'] = true
+      } else {
+        changed['checkboxItems[' + i + '].checked'] = false
+      }
+    }
+    this.setData(changed)
+  },
+
+  radioChange(e) {
+    const checked = e.detail.value
+    const changed = {}
+    for (let i = 0; i < this.data.radioItems.length; i++) {
+      if (checked.indexOf(this.data.radioItems[i].name) !== -1) {
+        changed['radioItems[' + i + '].checked'] = true
+      } else {
+        changed['radioItems[' + i + '].checked'] = false
+      }
+    }
+    this.setData(changed)
+  },
+
+  tapEvent() {
+    console.log('按钮被点击')
+  },
+  // 投票
+  next(){
+    this.setData({
+      isvote:false
+    })
   }
 })
